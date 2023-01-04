3c1eebd02a7e89c35505980fb8b8776156547fe7
Jozef Wells <97574935+jozefwells@users.noreply.github.com>
2023-01-04 16:39:58 -0700

    Add files via upload

---
+++ Java BTree/src/main/java/cs321/create/SequenceUtils.java
@@ -0,0 +1,28 @@
+package cs321.create;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility methods dealing with DNA sequences and its compact representation as long variables.
+ */
+public class SequenceUtils
+{
+
+	public static long DNAStringToLong(String DNAString)  {
+		return 0;
+	}
+
+
+	public String longToDNAString(long sequence, int seqLength) {
+		return null;
+	}
+
+
+	public static long getComplement(long sequence, int seqLength) {
+		return 0;
+	}
+
+}
